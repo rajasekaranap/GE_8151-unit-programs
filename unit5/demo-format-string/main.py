968b91f07dcce31bdd4deac47f661e8cc1539f0b
Peter Evans <peter-evans@users.noreply.github.com>
2020-05-09 08:01:37 +0000

    autopep8 action fixes

--- unit5/demo-format-string/main.py
+++ unit5/demo-format-string/main.py
@@ -1,6 +1,6 @@
 print("\nFORMAT OPERATOR")
 print("------------------")
 line_to_print = "My name is {}"
-print("Before Format:",line_to_print)
+print("Before Format:", line_to_print)
 line_to_print = line_to_print.format("Shiva")
-print("After Format:",line_to_print)
+print("After Format:", line_to_print)
